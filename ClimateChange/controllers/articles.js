b25c876341c4369d41eb9f0fdbf9468c700b4fe5
Archana Yadawa <archana@Archanas-MacBook-Pro.local>
2019-10-03 00:13:04 -0700

    additional articles

--- ClimateChange/controllers/articles.js
+++ ClimateChange/controllers/articles.js
@@ -92,8 +92,103 @@ const allArticles = [
 		id:19,
 		name:'Earth’s Orbital Shifts May Have Triggered Ancient Global Warming', 
 		url:'https://www.scientificamerican.com/article/earths-orbital-shifts-may-have-triggered-ancient-global-warming/',
-	}, 	
-
+	}, 	{
+		id: 20,
+		name: 'Media’s social construction of environmental issues: focus on global warming – a comparative study',
+		url: 'https://www.emerald.com/insight/content/doi/10.1108/01443330310790327/full/html',
+	}, {
+		id: 21,
+		name: 'Rice yields decline with higher night temperature from global warming',
+		url: 'https://www.pnas.org/content/101/27/9971.short',
+	}, {
+		id: 22,
+		name: 'Phenology Under Global Warming',
+		url: 'https://science.sciencemag.org/content/327/5972/1461',
+	}, {
+		id: 23,
+		name: 'Fingerprints of global warming on wild animals and plants',
+		url: 'https://www.nature.com/articles/nature01333',
+	}, {
+		id: 24,
+		name: 'Balance as bias: global warming and the US prestige press',
+		url: 'sciencedirect.com/science/article/abs/pii/S0959378003000669',
+	}, {
+		id: 25,
+		name: 'Long-term strategies for mitigating global warming',
+		url: 'https://www.sciencedirect.com/science/article/abs/pii/036054429390019A',
+	}, {
+		id: 26,
+		name: 'Fossil fuel decarbonization technology for mitigating global warming',
+		url: 'https://www.sciencedirect.com/science/article/abs/pii/S0360319998001281',
+	}, {
+		id: 27,
+		name: 'Global warming benefits the small in aquatic ecosystems',
+		url: 'https://www.pnas.org/content/106/31/12788.short',
+	}, {
+		id: 28,
+		name: 'Global Warming and Global Dioxide Emission: An Empirical Study',
+		url: 'https://www.sciencedirect.com/science/article/pii/S0301479796900250',
+	}, {
+		id: 27,
+		name: 'Global warming in the twenty-first century: An alternative scenario',
+		url: 'https://www.pnas.org/content/97/18/9875.short',
+	}, {
+		id: 28,
+		name: 'Earth"s Atmosphere: A Multi-layered Cake',
+		url: 'https://climate.nasa.gov/news/2919/earths-atmosphere-a-multi-layered-cake/',
+	}, {
+		id: 29,
+		name: 'The Atmosphere: Earth"s Security Blanket',
+		url: 'https://climate.nasa.gov/news/2914/the-atmosphere-earths-security-blanket/',
+	}, {
+		id: 30,
+		name: '2019 Arctic Sea Ice Minimum Tied for Second Lowest on Record',
+		url: 'https://climate.nasa.gov/news/2913/2019-arctic-sea-ice-minimum-tied-for-second-lowest-on-record/',
+	}, {
+		id: 31,
+		name: 'Satellite Data Record Shows Climate Change"s Impact on Fires',
+		url: 'https://climate.nasa.gov/news/2912/satellite-data-record-shows-climate-changes-impact-on-fires/',
+	}, {
+		id: 32,
+		name: 'NASA"s ECOSTRESS Detects Amazon Fires from Space',
+		url: 'https://climate.nasa.gov/news/2909/nasas-ecostress-detects-amazon-fires-from-space/',
+	}, {
+		id: 33,
+		name: 'Landsat Illustrates Five Decades of Change to Greenland Glaciers',
+		url: 'https://climate.nasa.gov/news/2908/landsat-illustrates-five-decades-of-change-to-greenland-glaciers/',
+	}, {
+		id: 34,
+		name: 'NASA"s AIRS Maps Carbon Monoxide from Brazil Fires',
+		url: 'https://climate.nasa.gov/news/2907/nasas-airs-maps-carbon-monoxide-from-brazil-fires/',
+	}, {
+		id: 35,
+		name: 'Boreal Forest Fires Could Release Deep Soil Carbon',
+		url: 'https://climate.nasa.gov/news/2905/boreal-forest-fires-could-release-deep-soil-carbon/',
+	}, {
+		id: 36,
+		name: 'GRACE-FO Shows the Weight of Midwestern Floods',
+		url: 'https://climate.nasa.gov/news/2904/grace-fo-shows-the-weight-of-midwestern-floods/',
+	}, {
+		id: 37,
+		name: 'NASA Studies How Arctic Wildfires Change the World',
+		url: 'https://climate.nasa.gov/news/2902/nasa-studies-how-arctic-wildfires-change-the-world/',
+	}, {
+		id: 38,
+		name: 'NASA Gauges Plant Stress in Costa Rican Drought',
+		url: 'https://climate.nasa.gov/news/2901/nasa-gauges-plant-stress-in-costa-rican-drought/',
+	}, {
+		id: 39,
+		name: 'NASA Targets Coastal Ecosystems with New Space Sensor',
+		url: 'https://climate.nasa.gov/news/2900/nasa-targets-coastal-ecosystems-with-new-space-sensor/',
+	}, {
+		id: 40,
+		name: 'A Partnership Forged by Fire',
+		url: 'https://climate.nasa.gov/news/2899/a-partnership-forged-by-fire/',
+	}, {
+		id: 41,
+		name: 'NASA Tracks Wildfires from Above to Aid Firefighters Below',
+		url: 'https://climate.nasa.gov/news/2898/nasa-tracks-wildfires-from-above-to-aid-firefighters-below/',
+	}
 ]
 		
 	
